d361924964da0a3e03ab512e55f700166f4cb8f9
Kathoni <thrusherkeys@gmail.com>
2026-08-27 01:25:57 +0300

    alter and update

--- DDL/1.1basics.sql
+++ DDL/1.1basics.sql
@@ -1,4 +1,9 @@
-CREATE DATABASE jobs_mart;
+-- .read DDL/1.1basics.sql
+
+USE data_jobs;
+
+DROP DATABASE IF EXISTS jobs_mart;
+
 CREATE DATABASE IF NOT EXISTS jobs_mart;
 
 SHOW DATABASES;
@@ -13,13 +18,13 @@ FROM information_schema.schemata;
 
 USE jobs_mart;
 
-CREATE SCHEMA jobs_mart.staging;
+-- CREATE SCHEMA jobs_mart.staging;
 CREATE SCHEMA IF NOT EXISTS staging;
 
 -- DROP SCHEMA IF EXISTS staging;
 
 CREATE TABLE IF NOT EXISTS staging.preferred_roles(
-    role_id INT ,
+    role_id INT PRIMARY KEY,
     role_name VARCHAR(50) 
 );
 
@@ -30,3 +35,37 @@ WHERE table_catalog = 'jobs_mart';
 -- DROP TABLE preferred_roles;
 
 -- DROP TABLE IF EXISTS main.preferred_roles;
+
+INSERT INTO staging.preferred_roles (role_id,role_name)
+VALUES 
+     (1,'Data Engineer'),
+     (2,'Senior Data Engineer'),
+     (3,'Software Engineer');
+
+SELECT *
+FROM staging.preferred_roles;
+
+ALTER TABLE staging.preferred_roles
+ADD COLUMN preferred_role BOOLEAN;
+
+
+-- ALTER TABLE staging.preferred_roles
+-- DROP COLUMN preferred_role ;
+
+UPDATE staging.preferred_roles
+SET preferred_role = TRUE
+WHERE role_id = 1 OR role_id = 2 ;
+
+UPDATE staging.preferred_roles
+SET preferred_role = FALSE
+WHERE role_id = 3 ;
+ 
+ALTER TABLE staging.preferred_roles
+RENAME TO priority_roles;
+
+SELECT *
+FROM staging.priority_roles;
+
+ALTER TABLE staging.priority_roles
+RENAME COLUMN preferred_role TO priority_lvl;
+
