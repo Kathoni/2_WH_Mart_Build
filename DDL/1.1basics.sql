3ac1ecd03a9d953925b2805510ab0f6543f78815
Kathoni <thrusherkeys@gmail.com>
2026-08-26 22:27:44 +0300

    Add schema creation and management commands in 1.1basics.sql

--- DDL/1.1basics.sql
+++ DDL/1.1basics.sql
@@ -3,4 +3,30 @@ CREATE DATABASE IF NOT EXISTS jobs_mart;
 
 SHOW DATABASES;
 
-DROP DATABASE jobs_mart;
+-- DROP DATABASE jobs_mart;
+-- DROP DATABASE IF EXISTS jobs_mart;
+
+-- creating and droping schemas.
+
+SELECT *
+FROM information_schema.schemata;
+
+USE jobs_mart;
+
+CREATE SCHEMA jobs_mart.staging;
+CREATE SCHEMA IF NOT EXISTS staging;
+
+-- DROP SCHEMA IF EXISTS staging;
+
+CREATE TABLE staging.preferred_roles(
+    role_id INT ,
+    role_name VARCHAR(50) 
+);
+
+SELECT *
+FROM information_schema.tables
+WHERE table_catalog = 'jobs_mart';
+
+DROP TABLE preferred_roles;
+
+DROP TABLE IF EXISTS main.preferred_roles;
