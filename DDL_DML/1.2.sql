3c934e774d70a64d15faa23521691869bb314ebb
Kathoni <thrusherkeys@gmail.com>
2026-08-30 17:59:27 +0300

    Enhance SQL script 1.2.sql with data management operations: delete outdated postings, truncate table, and insert new job postings. Add count queries for validation.

--- DDL_DML/1.2.sql
+++ DDL_DML/1.2.sql
@@ -55,3 +55,43 @@ SELECT
 FROM senior_jobs_flat_temp
 GROUP BY job_title_short
 ORDER BY job_count DESC;
+
+SELECT COUNT(*) FROM staging.job_postings_flat;
+SELECT COUNT(*) FROM staging.priority_jobs_flat_view;
+SELECT COUNT(*) FROM senior_jobs_flat_temp;
+
+DELETE FROM staging.job_postings_flat
+WHERE job_posted_date < '2024-01-01';
+
+SELECT COUNT(*) FROM staging.job_postings_flat;
+SELECT COUNT(*) FROM staging.priority_jobs_flat_view;
+SELECT COUNT(*) FROM senior_jobs_flat_temp;
+
+TRUNCATE TABLE staging.job_postings_flat;
+
+INSERT INTO staging.job_postings_flat
+SELECT
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.job_title,
+    jpf.job_location,
+    jpf.job_via,
+    jpf.job_schedule_type,
+    jpf.job_work_from_home,
+    jpf.search_location,
+    jpf.job_posted_date,
+    jpf.job_no_degree_mention,
+    jpf.job_health_insurance,
+    jpf.job_country,
+    jpf.salary_rate,
+    jpf.salary_year_avg,
+    jpf.salary_hour_avg,
+    cd.name,
+FROM data_jobs.job_postings_fact AS jpf
+LEFT JOIN data_jobs.company_dim AS cd
+    ON jpf.company_id = cd.company_id
+WHERE job_posted_date >= '2024-01-01';
+
+SELECT COUNT(*) FROM staging.job_postings_flat;
+SELECT COUNT(*) FROM staging.priority_jobs_flat_view;
+SELECT COUNT(*) FROM senior_jobs_flat_temp;
