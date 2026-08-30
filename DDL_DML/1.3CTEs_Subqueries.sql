97607335141a167e6a10844beb91b9a881eb2bd6
Kathoni <thrusherkeys@gmail.com>
2026-08-30 22:26:20 +0300

    Add SQL queries for subqueries and CTEs in 1.3CTEs_Subqueries.sql to enhance data retrieval and analysis

---
+++ DDL_DML/1.3CTEs_Subqueries.sql
@@ -0,0 +1,79 @@
+-- Subquery
+SELECT *
+FROM (
+    SELECT*
+    FROM job_postings_fact
+    WHERE salary_year_avg IS NOT NULL
+         OR salary_hour_avg IS NOT NULL
+) AS valid_salaries
+LIMIT 10;
+
+-- CTE
+WITH valid_salaries AS (
+ SELECT*
+    FROM job_postings_fact
+    WHERE salary_year_avg IS NOT NULL
+         OR salary_hour_avg IS NOT NULL
+)
+SELECT*
+FROM valid_salaries
+LIMIT 10;
+
+-- Scenario 1 - Subquery in SELECT
+-- Show each jobs salary next to overall maket median:
+SELECT 
+   job_title_short,
+   salary_year_avg,
+   (
+    SELECT MEDIAN(salary_year_avg)
+    FROM job_postings_fact
+   ) AS market_median_salary
+FROM  job_postings_fact 
+WHERE salary_year_avg IS NOT NULL
+LIMIT 10;  
+
+-- Scenario 2 - Subquery in FROM
+-- Show only jobs that are remote before aggregating
+SELECT 
+   job_title_short,
+   MEDIAN(salary_year_avg) AS median_salary,
+   (
+    SELECT MEDIAN(salary_year_avg)
+    FROM job_postings_fact
+    WHERE job_work_from_home = TRUE
+   ) AS market_remote_median_salary
+FROM (
+    SELECT 
+      job_title_short,
+      salary_year_avg
+    FROM job_postings_fact
+    WHERE job_work_from_home = TRUE
+) AS clean_jobs
+GROUP BY job_title_short
+LIMIT 10;  
+
+-- Scenario 3 - subquery in HAVING
+-- Keep only job titles whose median salary is above the overal median:
+
+SELECT 
+   job_title_short,
+   MEDIAN(salary_year_avg) AS median_salary,
+   (
+    SELECT MEDIAN(salary_year_avg)
+    FROM job_postings_fact
+    WHERE job_work_from_home = TRUE
+   ) AS market_remote_median_salary
+FROM (
+    SELECT 
+      job_title_short,
+      salary_year_avg
+    FROM job_postings_fact
+    WHERE job_work_from_home = TRUE
+) AS clean_jobs
+GROUP BY job_title_short
+HAVING MEDIAN(salary_year_avg) > (
+    SELECT MEDIAN(salary_year_avg)
+    FROM job_postings_fact
+    WHERE job_work_from_home = TRUE
+)
+LIMIT 10;  
