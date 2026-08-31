91597c2557f8f84ca587f4c6b4275a0ae2c9c7cf
Kathoni <thrusherkeys@gmail.com>
2026-08-31 11:44:44 +0300

    Fix SQL script to ensure priority_roles table is created only if it does not exist and correct the role_id for Software Engineer

--- DDL_DML/1.4_priority_roles.sql
+++ DDL_DML/1.4_priority_roles.sql
@@ -1,4 +1,4 @@
-CREATE TABLE staging.priority_roles(
+CREATE TABLE IF NOT EXISTS staging.priority_roles(
    role_id INTEGER PRIMARY KEY,
    role_name VARCHAR,
    priority_lvl INTEGER
@@ -8,6 +8,6 @@ INSERT INTO staging.priority_roles (role_id, role_name, priority_lvl)
 VALUES
 (1, 'Data Engineer', 1),
 (2, 'Senior Data Engineer', 1),
-(13, 'Software Engineer', 3);
+(3, 'Software Engineer', 3);
 
 SELECT * FROM staging.priority_roles;
