4642d0c20ecd7ff3db9b633351ec6391548f1344
Kathoni <thrusherkeys@gmail.com>
2026-08-31 14:25:46 +0300

    Add SQL scripts to manage priority_jobs_snapshot and priority_roles tables, including truncation, updates, and temporary table creation for job postings

--- DDL_DML/1.4_priority_roles.sql
+++ DDL_DML/1.4_priority_roles.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS priority_roles ;
 CREATE TABLE IF NOT EXISTS staging.priority_roles(
    role_id INTEGER PRIMARY KEY,
    role_name VARCHAR,
@@ -8,6 +9,6 @@ INSERT INTO staging.priority_roles (role_id, role_name, priority_lvl)
 VALUES
 (1, 'Data Engineer', 1),
 (2, 'Senior Data Engineer', 1),
-(3, 'Software Engineer', 3);
+(3, 'Software Engineer', 1);
 
 SELECT * FROM staging.priority_roles;
