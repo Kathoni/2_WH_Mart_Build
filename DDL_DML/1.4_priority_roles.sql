1d602ab9d1150de719a2c0a82e18428321b918b6
Kathoni <thrusherkeys@gmail.com>
2026-09-04 22:35:52 +0300

    fix: Remove Data Science and Senior Software Engineer entries from priority_roles table

--- DDL_DML/1.4_priority_roles.sql
+++ DDL_DML/1.4_priority_roles.sql
@@ -8,8 +8,6 @@ INSERT INTO staging.priority_roles (role_id, role_name, priority_lvl)
 VALUES
 (1, 'Data Engineer', 1),
 (2, 'Senior Data Engineer', 1),
-(3, 'Software Engineer', 3),
-(4, 'Data  Science', 2),
-(5, 'Senior Software Engineer', 2);
+(3, 'Software Engineer', 3),;
 
 SELECT * FROM staging.priority_roles;
