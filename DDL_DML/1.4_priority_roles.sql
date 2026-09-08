77d7afbe6e5f7dfc57b6bd0fb37e2a693a151eb2
Kathoni <thrusherkeys@gmail.com>
2026-09-08 19:47:39 +0300

    fix: Update priority level for Data Engineer and refine SQL statements in priority_jobs_snapshot

--- DDL_DML/1.4_priority_roles.sql
+++ DDL_DML/1.4_priority_roles.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE TABLE staging.priority_roles(
 
 INSERT INTO staging.priority_roles (role_id, role_name, priority_lvl)
 VALUES
-(1, 'Data Engineer', 1),
+(1, 'Data Engineer', 2),
 (2, 'Senior Data Engineer', 1),
 (3, 'Software Engineer', 3),
 (4, 'Senior Software Engineer', 3),
--- DDL_DML/priority_jobs_snapshot.sql
+++ DDL_DML/priority_jobs_snapshot.sql
@@ -15,17 +15,60 @@ LEFT JOIN data_jobs.company_dim AS cd
 INNER JOIN staging.priority_roles AS r
      ON jpf.job_title_short = r.role_name ;               
 
--- UPDATE Statement
-UPDATE main.priority_jobs_snapshot AS tgt
-SET 
-  priority_lvl = src.priority_lvl,
-  updated_at = src.updated_at
-FROM src_priority_jobs AS src  
-WHERE tgt.job_id = src.job_id
-     AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl;
+-- -- UPDATE Statement
+-- UPDATE main.priority_jobs_snapshot AS tgt
+-- SET 
+--   priority_lvl = src.priority_lvl,
+--   updated_at = src.updated_at
+-- FROM src_priority_jobs AS src  
+-- WHERE tgt.job_id = src.job_id
+--      AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl;
 
--- INSERT Statement
-INSERT INTO main.priority_jobs_snapshot (
+-- -- INSERT Statement
+-- INSERT INTO main.priority_jobs_snapshot (
+--     job_id,
+--     job_title_short,
+--     company_name, 
+--     job_posted_date,
+--     salary_year_avg ,  
+--     priority_lvl,
+--     updated_at
+-- )
+-- SELECT 
+--     src.job_id,
+--     src.job_title_short,
+--     src.company_name, 
+--     src.job_posted_date,
+--     src.salary_year_avg ,  
+--     src.priority_lvl,
+--     src.updated_at
+-- FROM src_priority_jobs AS src
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM main.priority_jobs_snapshot AS tgt
+--     WHERE tgt.job_id = src.job_id
+-- );   
+ 
+-- -- DELETE statement
+-- DELETE FROM main.priority_jobs_snapshot AS tgt
+-- WHERE NOT EXISTS (
+--    SELECT 1
+--    FROM src_priority_jobs AS src
+--    WHERE src.job_id = tgt.job_id
+-- );
+
+-- Merge into
+MERGE INTO main.priority_jobs_snapshot AS tgt
+USING src_priority_jobs AS src
+ON tgt.job_id = src.job_id
+
+WHEN MATCHED AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl THEN
+    UPDATE SET
+        priority_lvl = src.priority_lvl,
+        updated_at = src.updated_at
+
+WHEN NOT MATCHED THEN
+INSERT(
     job_id,
     job_title_short,
     company_name, 
@@ -34,7 +77,7 @@ INSERT INTO main.priority_jobs_snapshot (
     priority_lvl,
     updated_at
 )
-SELECT 
+VALUES ( 
     src.job_id,
     src.job_title_short,
     src.company_name, 
@@ -42,21 +85,8 @@ SELECT
     src.salary_year_avg ,  
     src.priority_lvl,
     src.updated_at
-FROM src_priority_jobs AS src
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM main.priority_jobs_snapshot AS tgt
-    WHERE tgt.job_id = src.job_id
-);   
- 
--- DELETE statement
-DELETE FROM main.priority_jobs_snapshot AS tgt
-WHERE NOT EXISTS (
-   SELECT 1
-   FROM src_priority_jobs AS src
-   WHERE src.job_id = tgt.job_id
-);
-
+)
+WHEN NOT MATCHED BY SOURCE THEN DELETE;
 -- Final Check Query
 SELECT 
     job_title_short,
