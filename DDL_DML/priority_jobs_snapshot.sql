f882043aca6514e03ed7f0ef72bb9af119dffe81
Kathoni <thrusherkeys@gmail.com>
2026-09-10 20:40:32 +0300

    fix: Refactor SQL statements in priority_jobs_snapshot for clarity and consistency

--- DDL_DML/priority_jobs_snapshot.sql
+++ DDL_DML/priority_jobs_snapshot.sql
@@ -15,60 +15,17 @@ LEFT JOIN data_jobs.company_dim AS cd
 INNER JOIN staging.priority_roles AS r
      ON jpf.job_title_short = r.role_name ;               
 
--- -- UPDATE Statement
--- UPDATE main.priority_jobs_snapshot AS tgt
--- SET 
---   priority_lvl = src.priority_lvl,
---   updated_at = src.updated_at
--- FROM src_priority_jobs AS src  
--- WHERE tgt.job_id = src.job_id
---      AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl;
+-- UPDATE Statement
+UPDATE main.priority_jobs_snapshot AS tgt
+SET 
+  priority_lvl = src.priority_lvl,
+  updated_at = src.updated_at
+FROM src_priority_jobs AS src  
+WHERE tgt.job_id = src.job_id
+     AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl;
 
--- -- INSERT Statement
--- INSERT INTO main.priority_jobs_snapshot (
---     job_id,
---     job_title_short,
---     company_name, 
---     job_posted_date,
---     salary_year_avg ,  
---     priority_lvl,
---     updated_at
--- )
--- SELECT 
---     src.job_id,
---     src.job_title_short,
---     src.company_name, 
---     src.job_posted_date,
---     src.salary_year_avg ,  
---     src.priority_lvl,
---     src.updated_at
--- FROM src_priority_jobs AS src
--- WHERE NOT EXISTS (
---     SELECT 1
---     FROM main.priority_jobs_snapshot AS tgt
---     WHERE tgt.job_id = src.job_id
--- );   
- 
--- -- DELETE statement
--- DELETE FROM main.priority_jobs_snapshot AS tgt
--- WHERE NOT EXISTS (
---    SELECT 1
---    FROM src_priority_jobs AS src
---    WHERE src.job_id = tgt.job_id
--- );
-
--- Merge into
-MERGE INTO main.priority_jobs_snapshot AS tgt
-USING src_priority_jobs AS src
-ON tgt.job_id = src.job_id
-
-WHEN MATCHED AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl THEN
-    UPDATE SET
-        priority_lvl = src.priority_lvl,
-        updated_at = src.updated_at
-
-WHEN NOT MATCHED THEN
-INSERT(
+-- INSERT Statement
+INSERT INTO main.priority_jobs_snapshot (
     job_id,
     job_title_short,
     company_name, 
@@ -77,7 +34,7 @@ INSERT(
     priority_lvl,
     updated_at
 )
-VALUES ( 
+SELECT 
     src.job_id,
     src.job_title_short,
     src.company_name, 
@@ -85,8 +42,51 @@ VALUES (
     src.salary_year_avg ,  
     src.priority_lvl,
     src.updated_at
-)
-WHEN NOT MATCHED BY SOURCE THEN DELETE;
+FROM src_priority_jobs AS src
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM main.priority_jobs_snapshot AS tgt
+    WHERE tgt.job_id = src.job_id
+);   
+ 
+-- DELETE statement
+DELETE FROM main.priority_jobs_snapshot AS tgt
+WHERE NOT EXISTS (
+   SELECT 1
+   FROM src_priority_jobs AS src
+   WHERE src.job_id = tgt.job_id
+);
+
+-- Merge into
+-- MERGE INTO main.priority_jobs_snapshot AS tgt
+-- USING src_priority_jobs AS src
+-- ON tgt.job_id = src.job_id
+
+-- WHEN MATCHED AND tgt.priority_lvl IS DISTINCT FROM src.priority_lvl THEN
+--     UPDATE SET
+--         priority_lvl = src.priority_lvl,
+--         updated_at = src.updated_at
+
+-- WHEN NOT MATCHED THEN
+-- INSERT(
+--     job_id,
+--     job_title_short,
+--     company_name, 
+--     job_posted_date,
+--     salary_year_avg ,  
+--     priority_lvl,
+--     updated_at
+-- )
+-- VALUES ( 
+--     src.job_id,
+--     src.job_title_short,
+--     src.company_name, 
+--     src.job_posted_date,
+--     src.salary_year_avg ,  
+--     src.priority_lvl,
+--     src.updated_at
+-- )
+-- WHEN NOT MATCHED BY SOURCE THEN DELETE;
 -- Final Check Query
 SELECT 
     job_title_short,
