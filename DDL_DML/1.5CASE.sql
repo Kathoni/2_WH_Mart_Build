e55f2a9e3b96f3ef3389a18a8233ad4698a19043
Kathoni <thrusherkeys@gmail.com>
2026-09-11 21:59:17 +0300

    fix: Update salary bucket comments for clarity in 1.5CASE.sql

--- DDL_DML/1.5CASE.sql
+++ DDL_DML/1.5CASE.sql
@@ -2,3 +2,15 @@
 -- <25 = Low
 -- 25-50 = Medium
 -- ? 50 = High
+
+SELECT 
+   job_title_short,
+   salary_hour_avg,
+   CASE
+         WHEN salary_hour_avg < 25 THEN 'Low'
+         WHEN salary_hour_avg >= 25 AND salary_hour_avg < 50 THEN 'Medium'
+         ELSE 'High'
+    END AS salary_category
+FROM data_jobs.job_postings_fact
+WHERE salary_hour_avg IS NOT NULL
+LIMIT 10;    
