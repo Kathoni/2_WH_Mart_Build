cefbe53c161d74def7d07c0e3ccc4e5c8c59000f
Kathoni <thrusherkeys@gmail.com>
2026-09-14 21:32:11 +0300

    feat: Add EXTRACT function to retrieve year, month, and day from job posted date

--- DDL_DML/1.6Date_functions.sql
+++ DDL_DML/1.6Date_functions.sql
@@ -6,3 +6,12 @@ SELECT
     job_posted_date::TIMESTAMPTZ AS timestamptz
 FROM job_postings_fact
 LIMIT 10;
+
+-- EXTRACT function
+SELECT 
+    job_posted_date,
+    EXTRACT(YEAR FROM job_posted_date) AS job_posted_year,
+    EXTRACT(MONTH FROM job_posted_date) AS job_posted_month,
+    EXTRACT(DAY FROM job_posted_date) AS job_posted_day
+FROM job_postings_fact
+LIMIT 10;    
