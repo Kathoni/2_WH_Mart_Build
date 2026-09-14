c513aa62e11fef92b7f68cdce224bb68e6562347
Kathoni <thrusherkeys@gmail.com>
2026-09-14 19:55:06 +0300

    feat: Expand SQL query to include multiple date formats for job posted date

--- DDL_DML/1.6Date_functions.sql
+++ DDL_DML/1.6Date_functions.sql
@@ -1,3 +1,8 @@
-SELECT job_posted_date
+SELECT 
+    job_posted_date,
+    job_posted_date::DATE AS date,
+    job_posted_date::TIME AS time,
+    job_posted_date::TIMESTAMP AS timestamp,
+    job_posted_date::TIMESTAMPTZ AS timestamptz
 FROM job_postings_fact
 LIMIT 10;
