9a68acdc331c7f1184b5ec3ac412470bf1331def
Kathoni <thrusherkeys@gmail.com>
2026-09-17 04:54:08 +0300

    feat: Add query to count job postings for Data Engineer in 2024 by month

--- DDL_DML/1.6Date_functions.sql
+++ DDL_DML/1.6Date_functions.sql
@@ -30,7 +30,19 @@ SELECT
     DATE_TRUNC('week', job_posted_date) AS truncated_week,
     DATE_TRUNC('day', job_posted_date) AS truncated_day,
     DATE_TRUNC('hour', job_posted_date) AS truncated_hour
-
 FROM job_postings_fact
 ORDER BY RANDOM()
 LIMIT 10;    
+
+SELECT 
+    DATE_TRUNC('month', job_posted_date) AS job_posted_month,
+    COUNT(job_id) AS job_count
+FROM job_postings_fact
+WHERE 
+    job_title_short = 'Data Engineer' AND 
+    EXTRACT(YEAR FROM job_posted_date) = 2024
+GROUP BY
+     DATE_TRUNC('month', job_posted_date)
+ORDER BY  
+     job_posted_month   
+;    
